1c1ce11cb01ec38e14b0b167f0381113a8260eee
lakhinsu <hinsulak@gmail.com>
2020-03-08 18:03:18 +0530

    Stable build 1

--- app/src/main/java/com/example/imazeandroid/common_screen.java
+++ app/src/main/java/com/example/imazeandroid/common_screen.java
@@ -1,6 +1,8 @@
 package com.example.imazeandroid;
 
+import android.app.ProgressDialog;
 import android.media.MediaPlayer;
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 import com.google.android.material.appbar.CollapsingToolbarLayout;
@@ -10,11 +12,15 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.util.Log;
 import android.view.View;
 import android.widget.VideoView;
 
+import java.util.List;
+
 public class common_screen extends AppCompatActivity {
 
+    ProgressDialog p;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,11 +38,43 @@ public class common_screen extends AppCompatActivity {
         String url = eventResourceMapper.getVideoURL();
         videoView.setVideoPath(url);
         videoView.start();
+
         videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
             @Override
             public void onPrepared(MediaPlayer mp) {
                 mp.setLooping(true);
             }
         });
+
+        LoadData loadData = new LoadData();
+        loadData.execute();
+
+
+    }
+    private class LoadData extends AsyncTask{
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            p = new ProgressDialog(common_screen.this);
+            p.setMessage("Loading Data ...");
+            p.setIndeterminate(false);
+            p.setCancelable(false);
+            p.show();
+        }
+
+        @Override
+        protected Event doInBackground(Object[] objects) {
+            SpreadSheetApi spreadSheetApi = new SpreadSheetApi(getApplicationContext(), "1vVa_nPyQEbVtdDL6zdnWZ8pgxBnGQhqFNuIxfUOCJ04");
+            Event event = spreadSheetApi.getSheet();
+            return event;
+        }
+
+        @Override
+        protected void onPostExecute(Object o) {
+            p.hide();
+            Event event = (Event) o;
+            Log.d("spreadsheet",event.getEvent_description());
+            super.onPostExecute(o);
+        }
     }
 }
